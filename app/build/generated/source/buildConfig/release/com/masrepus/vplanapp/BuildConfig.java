34a0b129364218355697983d76da6a216fad4a61
Samuel <masrepus97@gmail.com>
2014-12-12 19:14:21 +0100

    version bump for public release

--- app/build/generated/source/buildConfig/release/com/masrepus/vplanapp/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/com/masrepus/vplanapp/BuildConfig.java
@@ -5,9 +5,9 @@ package com.masrepus.vplanapp;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String PACKAGE_NAME = "com.masrepus.vplanapp";
+  public static final String APPLICATION_ID = "com.masrepus.vplanapp";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 5;
+  public static final int VERSION_CODE = 7;
   public static final String VERSION_NAME = "";
 }
