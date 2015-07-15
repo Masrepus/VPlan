1c0571749fbb786d5baf31f5a36747ce8144e7d5
Samuel <masrepus97@gmail.com>
2015-07-15 17:40:39 +0200

    version bump for public release

--- app/src/main/java/com/masrepus/vplanapp/vplan/MainActivity.java
+++ app/src/main/java/com/masrepus/vplanapp/vplan/MainActivity.java
@@ -21,6 +21,7 @@ import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
@@ -919,7 +920,7 @@ public class MainActivity extends ActionBarActivity implements SharedPreferences
             @Override
             public void onClick(View v) {
                 DrawerLayout layout = (DrawerLayout) findViewById(R.id.drawer_layout);
-                layout.openDrawer(Gravity.START);
+                layout.openDrawer(GravityCompat.START);
             }
         });
         showcase.setBlocksTouches(false);
